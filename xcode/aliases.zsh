265d603e242708ecb30e446a8b6b38dcf5b4e078
François Benaiteau <francois.benaiteau@sinnerschrader-mobile.com>
2014-06-20 16:07:27 +0200

    add Xcode beta alias

--- xcode/aliases.zsh
+++ xcode/aliases.zsh
@@ -2,6 +2,8 @@ alias ios="open /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimul
 
 # Open the current directory with Xcode and let it do its thang
 alias x="open -a 'Xcode' ."
+alias x-pre="open -a 'Xcode6-Beta' ."
+alias play="open -a 'Xcode6-Beta' ~/code/scratchpad.playground"
 
 # Clear the derived data folder cause uuuuuuggggghhhhhhh
 alias fuxcode='rm -rf ~/Library/Developer/Xcode/DerivedData/'
