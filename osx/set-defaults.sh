70baabb29a7cd6142458df24d4ee3d7ab565e67e
netbe <francois.benaiteau@gmail.com>
2015-06-29 20:52:55 +0200

    Disable dashboard

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -46,7 +46,7 @@ defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
 
 # Require password immediately after sleep or screen saver begins
 defaults write com.apple.screensaver askForPassword -int 1
-defaults write com.apple.screensaver askForPasswordDelay -int 0
+defaults write com.apple.screensaver askForPasswordDelay -int 30
 
 # Save screenshots to the desktop
 defaults write com.apple.screencapture location -string "${HOME}/Desktop"
@@ -87,3 +87,10 @@ sudo ln -sf /Applications/Xcode.app/Contents/Applications/iPhone\ Simulator.app 
 
 # Prevent Time Machine from prompting to use new hard drives as backup volume
 defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+###############################################################################
+# Spaces                                                                      #
+###############################################################################
+
+# Disable dashboard
+defaults write com.apple.dashboard mcx-disabled -boolean YES
