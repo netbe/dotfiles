6440c027a544831a774fe823123b508f8e87982c
François Benaiteau <francois.benaiteau@gmail.com>
2016-08-24 21:39:02 +0200

    xcode version

--- xcode/aliases.zsh
+++ xcode/aliases.zsh
@@ -1,8 +1,8 @@
 alias ios="open /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/Applications/iPhone\ Simulator.app"
 
 # Open the current directory with Xcode and let it do its thang
-alias x="open -a /Applications/Xcode-7.2.1.app ."
-alias play="open -a 'Xcode-7.2.1' ~/code/scratchpad.playground"
+alias x="open -a /Applications/Xcode.app ."
+alias play="open -a 'Xcode' ~/code/scratchpad.playground"
 
 # Clear the derived data folder cause uuuuuuggggghhhhhhh
 alias fuxcode='rm -rf ~/Library/Developer/Xcode/DerivedData/'
