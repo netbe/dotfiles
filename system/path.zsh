30c361eb8822fe18bcbc425bd83295e9213fe393
François Benaiteau <francois.benaiteau@gmail.com>
2016-08-24 21:38:04 +0200

    path change

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
-
+export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:/usr/local/go/bin:$HOME/.node/bin:$PATH"
+export PATH="$HOME/.npm-packages/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
