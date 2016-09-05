433e25d43eb8cac43316000e792afa8a89cbe098
François Benaiteau <francois.benaiteau@sharecare.com>
2016-09-05 10:44:26 +0200

    Update x alias to use xcenv

--- xcode/aliases.zsh
+++ xcode/aliases.zsh
@@ -1,8 +1,8 @@
 alias ios="open /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/Applications/iPhone\ Simulator.app"
 
 # Open the current directory with Xcode and let it do its thang
-alias x="open -a /Applications/Xcode.app ."
-alias play="open -a 'Xcode' ~/code/scratchpad.playground"
+alias play="xcenv open  ~/code/scratchpad.playground"
+alias x="xcenv open ."
 
 # Clear the derived data folder cause uuuuuuggggghhhhhhh
 alias fuxcode='rm -rf ~/Library/Developer/Xcode/DerivedData/'
